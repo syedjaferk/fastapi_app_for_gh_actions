bedbf4e9aa0de3c6ed81d34dee97330cb0a866c7
syedjaferk <learnwithjafer@gmail.com>
2026-06-23 20:24:29 +0530

    test: updated test

--- app/test_main.py
+++ app/test_main.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello FastAPI"}
+    assert response.json() == {"message": "Hello FastAPI From Syed Jafer"}
